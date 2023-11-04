a05620d93994ccc77451a6329dc7ea9a1e6615c5
Ola_Java <aersabanal@gmail.com>
2023-11-05 02:15:02 +0800

    commit ng leader

---
+++ script.js
@@ -0,0 +1,26 @@
+const listItems = document.querySelectorAll(".sidebar-list li");
+
+listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    let isActive = item.classList.contains("active");
+
+    listItems.forEach((el) => {
+      el.classList.remove("active");
+    });
+
+    if (isActive) item.classList.remove("active");
+    else item.classList.add("active");
+  });
+});
+
+const toggleSidebar = document.querySelector(".toggle-sidebar");
+const logo = document.querySelector(".logo-box");
+const sidebar = document.querySelector(".sidebar");
+
+toggleSidebar.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+logo.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
